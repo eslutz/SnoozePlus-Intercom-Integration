53b0ce6bb1a8ebe4e827e818d4c3eabe5f0302f7
Eric Slutz <eric.slutz@icloud.com>
2024-08-23 06:30:45 -0400

    fix app widget url

--- src/index.js
+++ src/index.js
@@ -11,32 +11,30 @@ window.intercomSettings = {
 
 (function () {
   var w = window;
-  var ic = w.Intercom;
-  if (typeof ic === 'function') {
-    ic('reattach_activator');
-    ic('update', intercomSettings);
-  } else {
-    var d = document;
-    var i = function () {
+  var d = document;
+  class i {
+    constructor() {
       i.c(arguments);
-    };
-    i.q = [];
-    i.c = function (args) {
+    }
+    static c(args) {
       i.q.push(args);
-    };
-    w.Intercom = i;
-    function l() {
-      var s = d.createElement('script');
-      s.type = 'text/javascript';
-      s.async = true;
-      s.src = 'https://widget.intercom.io/widget/APP_ID';
-      var x = d.getElementsByTagName('script')[0];
-      x.parentNode.insertBefore(s, x);
     }
-    if (w.attachEvent) {
-      w.attachEvent('onload', l);
-    } else {
-      w.addEventListener('load', l, false);
-    }
+  }
+  i.q = [];
+  w.Intercom = i;
+  function l() {
+    console.log('function l');
+    console.log();
+    var s = d.createElement('script');
+    s.type = 'text/javascript';
+    s.async = true;
+    s.src = `https://widget.intercom.io/widget/${intercomSettings.app_id}`;
+    var x = d.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+  }
+  if (w.attachEvent) {
+    w.attachEvent('onload', l);
+  } else {
+    w.addEventListener('load', l, false);
   }
 })();
