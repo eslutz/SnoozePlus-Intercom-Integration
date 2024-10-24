5cd473e3a00544f0ed08a76ff999241a2c20949e
Eric Slutz <eric.slutz@icloud.com>
2024-10-24 15:59:34 -0400

    implement retry in heartbeat-service

--- src/config/retry-config.ts
+++ src/config/retry-config.ts
@@ -1,11 +1,17 @@
 import retry from 'retry';
 
+const retries = Number(process.env.RETRY_ATTEMPTS) ?? 3;
+const factor = Number(process.env.RETRY_FACTOR) ?? 2;
+const minTimeout = Number(process.env.RETRY_MIN_TIMEOUT) ?? 1000;
+const maxTimeout = Number(process.env.RETRY_MAX_TIMEOUT) ?? 5000;
+const randomize = Boolean(process.env.RETRY_RANDOMIZE) ?? true;
+
 const operation = retry.operation({
-  retries: 5,
-  factor: 2,
-  minTimeout: 1000,
-  maxTimeout: 5000,
-  randomize: true,
+  retries: retries,
+  factor: factor,
+  minTimeout: minTimeout,
+  maxTimeout: maxTimeout,
+  randomize: randomize,
 });
 
 export default operation;
--- src/services/heartbeat-service.ts
+++ src/services/heartbeat-service.ts
@@ -3,21 +3,29 @@ const fetch = (...args) =>
   // @ts-expect-error: type not yet defined
   import('node-fetch').then(({ default: fetch }) => fetch(...args));
 import logger from '../config/logger-config';
+import operation from '../config/retry-config';
 
 const heartbeatUrl = process.env.BETTERSTACK_HEARTBEAT_URL ?? '';
 
 const sendHeartbeat = async (success: boolean = true): Promise<void> => {
   const url = success ? heartbeatUrl : `${heartbeatUrl}/fail`;
-  try {
-    const response = await fetch(url, { method: 'HEAD' });
-    if (response.ok) {
-      logger.info('Heartbeat sent successfully.');
-    } else {
-      logger.error(`Failed to send heartbeat. Status: ${response.status}`);
+  operation.attempt(async (currentAttempt) => {
+    try {
+      const response = await fetch(url, { method: 'HEAD' });
+      if (response.ok) {
+        logger.info('Heartbeat sent successfully.');
+      } else {
+        logger.error(`Failed to send heartbeat. Status: ${response.status}`);
+      }
+    } catch (err) {
+      logger.error(
+        `Error sending heartbeat on attempt ${currentAttempt}: ${err}`
+      );
+      if (operation.retry(err as Error)) {
+        return;
+      }
     }
-  } catch (error) {
-    logger.error(`Error sending heartbeat: ${error}`);
-  }
+  });
 };
 
 export { sendHeartbeat };
