1bfe5a9f78648410c2e9d120270d7ee2ae03269f
Eric Slutz <eric.slutz@icloud.com>
2024-11-13 06:58:43 -0500

    fix crypto error

--- src/utilities/crypto-utility.ts
+++ src/utilities/crypto-utility.ts
@@ -8,11 +8,10 @@ const key = process.env.ENCRYPTION_KEY;
 if (!key) {
   throw new Error('ENCRYPTION_KEY cannot be found!');
 }
-const keyBuffer = Buffer.from(key, 'hex');
 
 const encrypt = (text: string): string => {
   const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv(algorithm, keyBuffer, iv);
+  const cipher = crypto.createCipheriv(algorithm, Buffer.from(key, 'hex'), iv);
   let encrypted = cipher.update(text, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return `${iv.toString('hex')}:${encrypted}`;
@@ -22,7 +21,7 @@ const decrypt = (encryptedText: string): string => {
   const [ivHex, encrypted] = encryptedText.split(':');
   const cipher = crypto.createDecipheriv(
     algorithm,
-    keyBuffer,
+    Buffer.from(key, 'hex'),
     Buffer.from(ivHex, 'hex')
   );
   let decrypted = cipher.update(encrypted, 'hex', 'utf8');
