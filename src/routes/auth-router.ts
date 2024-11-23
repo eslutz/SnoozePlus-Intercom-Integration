e9782724e3b68335f1860cce7bfa76937f678e05
Eric Slutz <eric.slutz@icloud.com>
2024-11-23 05:39:01 -0500

    update to accept HttpPost OAuth calls

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -3,8 +3,14 @@ import * as authController from '../controllers/auth-controller';
 
 const authRouter = express.Router();
 
-authRouter.route('/intercom').get(authController.login);
-authRouter.route('/intercom/callback').get(authController.callback);
+authRouter
+  .route('/intercom')
+  .get(authController.login)
+  .post(authController.login);
+authRouter
+  .route('/intercom/callback')
+  .get(authController.callback)
+  .post(authController.callback);
 authRouter.route('/failure').get(authController.failure);
 authRouter.route('/logout').get(authController.logout);
 
