6094756a066347b6308e7363d1d018c3e22b21a1
Eric Slutz <eric.slutz@icloud.com>
2025-01-21 17:07:45 -0500

    resolve linting issues

--- src/services/heartbeat-service.ts
+++ src/services/heartbeat-service.ts
@@ -1,35 +1,47 @@
 import fetch from 'node-fetch';
 import config from '../config/config.js';
 import logger from '../config/logger-config.js';
-import operation from '../config/retry-config.js';
+import createRetryOperation from '../config/retry-config.js';
 
 const heartbeatLogger = logger.child({ module: 'heartbeat-service' });
 
 const baseUrl = config.betterstackHeartbeatUrl;
 
-const sendHeartbeat = async (success = true): Promise<void> => {
+const sendHeartbeat = (success = true): Promise<void> => {
   const url = success ? baseUrl : `${baseUrl}/fail`;
 
   return new Promise((resolve, reject) => {
-    operation.attempt(async (currentAttempt) => {
+    const operation = createRetryOperation();
+    operation.attempt((currentAttempt) => {
       try {
-        const response = await fetch(url, { method: 'HEAD' });
-        if (response.ok) {
-          heartbeatLogger.info('Heartbeat sent successfully.');
-        } else {
-          heartbeatLogger.error(
-            `Failed to send heartbeat. Status: ${response.status}`
-          );
-        }
-        resolve();
+        fetch(url, { method: 'HEAD' })
+          .then((response) => {
+            if (response.ok) {
+              heartbeatLogger.info('Heartbeat sent successfully.');
+            } else {
+              heartbeatLogger.error(
+                `Failed to send heartbeat. Status: ${response.status}`
+              );
+            }
+            resolve();
+          })
+          .catch((err: Error) => {
+            heartbeatLogger.error(
+              `Error sending heartbeat on attempt ${currentAttempt}: ${err.message}`
+            );
+            if (operation.retry(err)) {
+              return null;
+            }
+            reject(operation.mainError()!);
+          });
       } catch (err) {
         heartbeatLogger.error(
-          `Error sending heartbeat on attempt ${currentAttempt}: ${err}`
+          `Error sending heartbeat on attempt ${currentAttempt}: ${String(err)}`
         );
         if (operation.retry(err as Error)) {
-          return;
+          return null;
         }
-        reject(operation.mainError());
+        reject(operation.mainError()!);
       }
     });
   });
