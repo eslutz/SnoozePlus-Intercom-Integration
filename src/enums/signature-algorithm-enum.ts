b9f683a780b383848b4bb13f059297e33786cb3f
Eric Slutz <eric.slutz@icloud.com>
2025-01-22 06:43:51 -0500

    add documentation for SignatureAlgorithm enum

--- src/enums/signature-algorithm-enum.ts
+++ src/enums/signature-algorithm-enum.ts
@@ -1,3 +1,11 @@
+/**
+ * Represents the HMAC algorithms used for signature validation.
+ *
+ * @enum {string}
+ * @readonly
+ * @property {string} WEBHOOK SHA-1 algorithm used for webhook signatures
+ * @property {string} CANVAS SHA-256 algorithm used for canvas signatures
+ */
 enum SignatureAlgorithm {
   WEBHOOK = 'sha1',
   CANVAS = 'sha256',
